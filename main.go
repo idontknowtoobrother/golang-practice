6b4ddfcebb4cc4003136eb8132547589d202c822
idontknowtoobrother <idontknowbrotherpls@gmail.com>
2024-06-16 02:26:15 +0700

    pushed to my brain

--- main.go
+++ main.go
@@ -2,19 +2,26 @@ package main
 
 import "fmt"
 
-func createArray() *[5]int {
-	return &[5]int{}
-}
-
-func fillArray(arrs *[5]int) {
-	for i := range arrs {
-		arrs[i] = i + 1
+func increamentCount(mapItem map[string]*int, itemName string) {
+	if _, exist := mapItem[itemName]; !exist {
+		count := 0
+		mapItem[itemName] = &count
 	}
+
+	*mapItem[itemName]++
 }
 
 func main() {
-	arrs := createArray()
-	fillArray(arrs)
+	mapItem := make(map[string]*int)
 
-	fmt.Println("Array values:", *arrs)
+	increamentCount(mapItem, "apple")
+	increamentCount(mapItem, "apple")
+	increamentCount(mapItem, "apple")
+	increamentCount(mapItem, "apple")
+	increamentCount(mapItem, "orange")
+	increamentCount(mapItem, "orange")
+
+	for itemName, count := range mapItem {
+		fmt.Printf("\nitem: %s, count: %d", itemName, *count)
+	}
 }
