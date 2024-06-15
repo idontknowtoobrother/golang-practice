0518aafd42c90c47ca8efcd88b7a7a95313279d5
idontknowtoobrother <idontknowbrotherpls@gmail.com>
2024-06-16 02:18:24 +0700

    pushed to my brain

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/idontknowtoobrother/golang-practice/ptr-swap
+module github.com/idontknowtoobrother/golang-practice/ptr-function-return-pointer
 
 go 1.23
--- main.go
+++ main.go
@@ -2,23 +2,19 @@ package main
 
 import "fmt"
 
+func createArray() *[5]int {
+	return &[5]int{}
+}
+
+func fillArray(arrs *[5]int) {
+	for i := range arrs {
+		arrs[i] = i + 1
+	}
+}
+
 func main() {
+	arrs := createArray()
+	fillArray(arrs)
 
-	var arayyInteger [5]int = [5]int{
-		10,
-		20,
-		30,
-		40,
-		50,
-	}
-
-	arrayPtrInteger := [5]*int{}
-
-	for i := range arayyInteger {
-		arrayPtrInteger[i] = &arayyInteger[i]
-	}
-
-	for i := range arrayPtrInteger {
-		fmt.Printf("Value at index %d: %d, Address: %p\n", i, *arrayPtrInteger[i], arrayPtrInteger[i])
-	}
+	fmt.Println("Array values:", *arrs)
 }
