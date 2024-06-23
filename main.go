45d8da553f7256b7a5b3bb637783a57b3733fb0d
jakkritgridwhiz <jakkrit@gridwhiz.com>
2024-06-23 22:08:04 +0700

    asdasd

--- main.go
+++ main.go
@@ -13,6 +13,8 @@ func main() {
 	numAdd := calculator.Add(1, 2)
 	fmt.Println(numAdd)
 
+	//commented out because multiply is a private function
+
 	numMultiply := calculator.PublicMultiply(2, 3)
 	fmt.Println(numMultiply)
 
@@ -22,4 +24,9 @@ func main() {
 	})
 	e.Logger.Fatal(e.Start(":1323"))
 
+	numMultiply = calculator.PublicMultiply(2, 3)
+	fmt.Println(numMultiply)
+
+	numMultiply = calculator.PublicMultiply(2, 3)
+	fmt.Println(numMultiply)
 }
